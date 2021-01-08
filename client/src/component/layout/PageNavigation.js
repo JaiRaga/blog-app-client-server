5a7ce23ef881e93e50e5b00ce0a43444e0f783e0
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-08 11:47:23 +0530

    added button highlighting based on the url

--- client/src/component/layout/PageNavigation.js
+++ client/src/component/layout/PageNavigation.js
@@ -3,13 +3,15 @@ import { Grid, Button, makeStyles, Hidden, List, ListItem, ListItemText, ListIte
 import HomeSharpIcon from '@material-ui/icons/HomeSharp';
 import PersonSharpIcon from '@material-ui/icons/PersonSharp';
 import SettingsSharpIcon from '@material-ui/icons/SettingsSharp';
+import CreateBlogBtn from '../blogs/CreateBlogBtn';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         maxWidth: 360,
         // backgroundColor: theme.palette.background.paper,
-        backgroundColor: "#666"
+        // backgroundColor: "#666"
     },
     background: {
 
@@ -22,29 +24,53 @@ const useStyles = makeStyles(theme => ({
 
 const PageNavigation = () => {
     const classes = useStyles()
+    const history = useHistory()
+    const path = history.location.pathname
+    console.log(history)
     return (
         // <Grid item xs={0} sm={2} className={classes.background}>
         <Hidden only={['xs', 'sm']}>
             <Grid item className={classes.root}>
                 {/* <Grid container item direction="column" justify="center" alignItems="center"> */}
                 <List component="nav" aria-label="side-navigation" classNAme={classes.list}>
-                    <ListItem button>
+                    <ListItem button onClick={() => history.push("/dashboard")} selected={path === "/dashboard" ? true : false}>
                         <ListItemIcon>
                             <HomeSharpIcon />
                         </ListItemIcon>
                         <ListItemText primary="Home" />
                     </ListItem>
-                    <ListItem button>
+                    <ListItem button onClick={() => history.push("/profile")} selected={path === "/profile" ? true : false}>
                         <ListItemIcon>
                             <PersonSharpIcon />
                         </ListItemIcon>
                         <ListItemText primary="Profile" />
                     </ListItem>
-                    <ListItem button>
+                    <ListItem button onClick={() => history.push("/setting")} selected={path === "/setting" ? true : false}>
                         <ListItemIcon>
                             <SettingsSharpIcon />
                         </ListItemIcon>
                         <ListItemText primary="Settings" />
+                    </ListItem>
+                    <ListItem button onClick={() => history.push("/setting")} selected={path === "/0000" ? true : false}>
+                        <ListItemIcon>
+                            <HomeSharpIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Home" />
+                    </ListItem>
+                    <ListItem button onClick={() => history.push("/setting")} selected={path === "/0000" ? true : false}>
+                        <ListItemIcon>
+                            <PersonSharpIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Profile" />
+                    </ListItem>
+                    <ListItem button onClick={() => history.push("/setting")} selected={path === "/0000" ? true : false}>
+                        <ListItemIcon>
+                            <SettingsSharpIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Settings" />
+                    </ListItem>
+                    <ListItem>
+                        <CreateBlogBtn />
                     </ListItem>
                 </List>
             </Grid>
