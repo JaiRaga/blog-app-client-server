b862075cfc0975e3a4aa7caabd87f565abff0c0a
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-08 11:46:17 +0530

    added option to specify color

--- client/src/component/layout/GoBack.js
+++ client/src/component/layout/GoBack.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Grid, Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 
-const GoBack = ({ fullWidth }) => {
+const GoBack = ({ fullWidth, shade }) => {
   const history = useHistory();
   return (
     <Grid container justify='flex-start' item>
-      <Button fullWidth={fullWidth} onClick={() => history.goBack()}>
+      <Button fullWidth={fullWidth} onClick={() => history.goBack()} color={shade || null}>
         Go Back
       </Button>
     </Grid>
