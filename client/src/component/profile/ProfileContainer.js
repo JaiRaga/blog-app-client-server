df0ba3a353a951aa22458b63a8e8f5f894038de3
JaiRaga <raga.j.santhosh@gmail.com>
2021-01-08 11:48:07 +0530

    added page navigation component

--- client/src/component/profile/ProfileContainer.js
+++ client/src/component/profile/ProfileContainer.js
@@ -1,26 +1,46 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Hidden } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import Profile from "./Profile";
 import ProfileDisplay from "./ProfileDisplay";
+import PageNavigation from "../layout/PageNavigation"
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    display: "flex",
+    [theme.breakpoints.down("xs")]: {
+      justifyContent: "center"
+    }
+  },
   grid: {
     backgroundColor: "#999",
-    margin: 5,
+    // margin: 5,
     padding: 20
+  },
+  profileContent: {
+    marginLeft: 14
   }
 }));
 
 const ProfileContainer = () => {
   const classes = useStyles();
   return (
-    <Grid container justify='center'>
-      <Grid item xs={5} md={3} className={classes.grid}>
-        <Profile />
-      </Grid>
-      <Grid item className={classes.grid} xs={12} md={6}>
-        <ProfileDisplay />
+    <Grid container justify="center" className={classes.root}>
+      {/* <Grid item xs={0} md={3}> */}
+      {/* <Hidden only={['xs', 'sm', 'md']}> */}
+      <PageNavigation />
+      {/* </Hidden> */}
+      {/* </Grid> */}
+      <Grid item xs={12} sm={6} lg={8} className={classes.profileContent}>
+        <Grid container item>
+          <Grid item xs={5} md={3} className={classes.grid}>
+            <Profile />
+          </Grid>
+          <Grid item className={classes.grid} xs={12} md={6}>
+            <ProfileDisplay />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
