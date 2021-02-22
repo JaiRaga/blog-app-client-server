a8e3d3e2363ce4fa41ba8891e70e3299ccd001ea
JaiRaga <raga.j.santhosh@gmail.com>
2021-02-22 15:28:52 +0530

    created routes for tags page

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,63 @@
-import React, { useEffect } from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./component/routing/PrivateRoute";
-import Login from "./component/auth/Login";
-import Register from "./component/auth/Register";
-import Blogs from "./component/blogs/Blogs";
-import Blog from "./component/blogs/Blog";
-import Navbar from "./component/layout/Navbar";
-import Drawer from "./component/layout/Drawer";
-import Dashboard from "./component/dashboard/Dashboard";
-import TrendingSideBar from "./component/trending/TrendingSideBar";
-import TrendingBlogs from "./component/trending/TrendingBlogs";
-import ProfileContainer from "./component/profile/ProfileContainer";
-import Search from "./component/search/Search";
-import Setting from "./component/setting/Setting";
-import CreateBlog from "./component/blogs/CreateBlog";
+import React, { useEffect } from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PrivateRoute from './component/routing/PrivateRoute'
+import Login from './component/auth/Login'
+import Register from './component/auth/Register'
+import Blogs from './component/blogs/Blogs'
+import Blog from './component/blogs/Blog'
+import Navbar from './component/layout/Navbar'
+import Drawer from './component/layout/Drawer'
+import Dashboard from './component/dashboard/Dashboard'
+import TrendingSideBar from './component/trending/TrendingSideBar'
+import TrendingBlogs from './component/trending/TrendingBlogs'
+import ProfileContainer from './component/profile/ProfileContainer'
+import Search from './component/search/Search'
+import Setting from './component/setting/Setting'
+import CreateBlog from './component/blogs/CreateBlog'
+import Tags from './component/tag/Tags'
 
 // Redux
-import store from "./redux/store";
-import { loadUser } from "./redux/actions/auth";
-import { getBlogs } from "./redux/actions/blog";
+import store from './redux/store'
+import { loadUser } from './redux/actions/auth'
+import { getBlogs } from './redux/actions/blog'
 
 // utils
-import setAuthToken from "./utils/setAuthToken";
+import setAuthToken from './utils/setAuthToken'
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+	setAuthToken(localStorage.token)
 }
 
 function App() {
-  useEffect(() => {
-    store.dispatch(loadUser());
-    store.dispatch(getBlogs());
-  }, []);
-  return (
-    <Provider store={store}>
-      <Router>
-        <Navbar />
-        <Drawer />
-        <TrendingSideBar />
-        <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/search' component={Search} />
-          <PrivateRoute exact path='/create' component={CreateBlog} />
-          <PrivateRoute exact path='/dashboard' component={Dashboard} />
-          <PrivateRoute exact path='/blog' component={Blog} />
-          <PrivateRoute exact path='/blogs' component={Blogs} />
-          <PrivateRoute exact path='/profile' component={ProfileContainer} />
-          <PrivateRoute exact path='/setting' component={Setting} />
+	useEffect(() => {
+		store.dispatch(loadUser())
+		store.dispatch(getBlogs())
+	}, [])
 
-          {/* <Route exact path='/trending' component={TrendingBlogs} /> */}
-        </Switch>
-      </Router>
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Router>
+				<Navbar />
+				<Drawer />
+				<TrendingSideBar />
+				<Switch>
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/register' component={Register} />
+					<Route exact path='/search' component={Search} />
+					<PrivateRoute exact path='/create' component={CreateBlog} />
+					<PrivateRoute exact path='/dashboard' component={Dashboard} />
+					<PrivateRoute exact path='/blog/:id' component={Blog} />
+					<PrivateRoute exact path='/blogs' component={Blogs} />
+					<PrivateRoute exact path='/profile' component={ProfileContainer} />
+					<PrivateRoute exact path='/tags' component={Tags} />
+					<PrivateRoute exact path='/setting' component={Setting} />
+
+					{/* <Route exact path='/trending' component={TrendingBlogs} /> */}
+				</Switch>
+			</Router>
+		</Provider>
+	)
 }
 
-export default App;
+export default App
