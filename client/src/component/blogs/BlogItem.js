901e31db54545888590c960182d4cc9a9b12ff04
JaiRaga <raga.j.santhosh@gmail.com>
2021-02-22 15:12:04 +0530

    changed cursor to pointer

--- client/src/component/blogs/BlogItem.js
+++ client/src/component/blogs/BlogItem.js
@@ -7,6 +7,7 @@ import {
   makeStyles,
   Divider
 } from "@material-ui/core";
+import { useHistory } from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -16,7 +17,8 @@ const useStyles = makeStyles((theme) => ({
       "linear-gradient(90deg, rgba(16,3,158,1) 0%, rgba(9,73,121,1) 15%, rgba(4,204,245,1) 48%)"
   },
   title: {
-    padding: "20px 0"
+    padding: "20px 0",
+    cursor: 'pointer'
   },
   tags: {
     padding: "20px 0"
@@ -25,13 +27,14 @@ const useStyles = makeStyles((theme) => ({
 
 const BlogItem = ({ blog, trending }) => {
   const classes = useStyles();
+  const history = useHistory()
   const mediumScreenTitle = trending ? 12 : 8;
   const mediumScreenTags = trending ? 12 : 4;
 
   return (
     <Fragment>
       <Grid container item justify='center' className={classes.root}>
-        <Grid item xs={12} md={mediumScreenTitle} className={classes.title}>
+        <Grid item xs={12} md={mediumScreenTitle} className={classes.title} onClick={() => history.push(`/blog/${blog._id}`)}>
           <Typography variant='h4' align='center'>
             {blog.title}
           </Typography>
@@ -39,7 +42,7 @@ const BlogItem = ({ blog, trending }) => {
         <Grid item xs={12} md={mediumScreenTags} className={classes.tags}>
           <Grid container item justify='center'>
             {blog.category.map((tag) => (
-              <Button key={uuidv4()} variant='contained' color='primary'>
+              <Button key={uuidv4()} variant='contained' color='primary' onClick={() => history.push("/tags")}>
                 {tag}
               </Button>
             ))}
