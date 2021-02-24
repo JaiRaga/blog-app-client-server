db5935c6ea52e2f85f2872407ff2bf06e980acf2
JaiRaga <raga.j.santhosh@gmail.com>
2021-02-24 14:56:17 +0530

    removed commented out code

--- client/src/component/profile/ProfileContainer.js
+++ client/src/component/profile/ProfileContainer.js
@@ -1,49 +1,53 @@
-import React from "react";
-import { Grid, Hidden } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
-import Profile from "./Profile";
-import ProfileDisplay from "./ProfileDisplay";
-import PageNavigation from "../layout/PageNavigation"
+import React, { useEffect } from 'react'
+import { Grid, Hidden } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { useDispatch } from 'react-redux'
+import Profile from './Profile'
+import ProfileDisplay from './ProfileDisplay'
+import PageNavigation from '../layout/PageNavigation'
+import { loadUser } from '../../redux/actions/auth'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    display: "flex",
-    [theme.breakpoints.down("xs")]: {
-      justifyContent: "center"
-    }
-  },
-  grid: {
-    backgroundColor: "#999",
-    // margin: 5,
-    padding: 20
-  },
-  profileContent: {
-    marginLeft: 14
-  }
-}));
+	root: {
+		flexGrow: 1,
+		display: 'flex',
+		[theme.breakpoints.down('xs')]: {
+			justifyContent: 'center',
+		},
+	},
+	grid: {
+		backgroundColor: '#999',
+		// margin: 5,
+		padding: 20,
+	},
+	profileContent: {
+		marginLeft: 0,
+	},
+}))
 
 const ProfileContainer = () => {
-  const classes = useStyles();
-  return (
-    <Grid container justify="center" className={classes.root}>
-      {/* <Grid item xs={0} md={3}> */}
-      {/* <Hidden only={['xs', 'sm', 'md']}> */}
-      <PageNavigation />
-      {/* </Hidden> */}
-      {/* </Grid> */}
-      <Grid item xs={12} sm={6} lg={8} className={classes.profileContent}>
-        <Grid container item>
-          <Grid item xs={5} md={3} className={classes.grid}>
-            <Profile />
-          </Grid>
-          <Grid item className={classes.grid} xs={12} md={6}>
-            <ProfileDisplay />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
-};
+	const classes = useStyles()
+	const dispatch = useDispatch()
 
-export default ProfileContainer;
+	useEffect(() => {
+		dispatch(loadUser())
+	}, [])
+
+	return (
+		<Grid container justify='center' className={classes.root}>
+			<PageNavigation />
+			<Grid item xs={12} sm={6} lg={8} className={classes.profileContent}>
+				<Grid container item justify='center'>
+					<Grid item xs={5} md={3} className={classes.grid}>
+						<Profile />
+					</Grid>
+					<Grid item className={classes.grid} xs={12} md={6}>
+						<ProfileDisplay />
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	)
+}
+
+export default ProfileContainer
