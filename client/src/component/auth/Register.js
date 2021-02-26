ee66f4bf5464081ac46b5f3ae353094923f22f3d
JaiRaga <raga.j.santhosh@gmail.com>
2021-02-26 12:13:59 +0530

    changed layout

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,144 +1,203 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
-  makeStyles,
-  Grid,
-  TextField,
-  Button,
-  Typography
-} from "@material-ui/core";
-import { Link, Redirect } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { registerUser } from "../../redux/actions/auth";
+	makeStyles,
+	Grid,
+	TextField,
+	Button,
+	Typography,
+	Paper,
+} from '@material-ui/core'
+import { Link, Redirect } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { registerUser } from '../../redux/actions/auth'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      width: "25ch"
-    }
-  },
-  registerContainer: {
-    marginTop: "20px"
-  }
-}));
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			width: '25ch',
+		},
+	},
+	registerContainer: {
+		marginTop: '20px',
+	},
+	typography: {
+		color: '#fff',
+	},
+	textfieldColor: {
+		backgroundColor: '#111d5e',
+		color: '#000',
+	},
+	parentGrid: {
+		marginTop: 10,
+	},
+	paper: {
+		padding: '20px 0',
+		backgroundColor: '#111d5e99',
+	},
+	link: {
+		color: '#0a043c',
+		fontFamily: 'monospace',
+		fontSize: 19,
+		display: 'block',
+	},
+	footer: {
+		marginTop: '20px',
+		color: '#fff',
+	},
+}))
 
 const Login = () => {
-  const classes = useStyles();
+	const classes = useStyles()
 
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-  const dispatch = useDispatch();
+	const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+	const dispatch = useDispatch()
 
-  const [registerState, setRegisterState] = useState({
-    username: "",
-    email: "",
-    password: "",
-    password2: "",
-    handle: ""
-  });
+	const [registerState, setRegisterState] = useState({
+		username: '',
+		email: '',
+		password: '',
+		password2: '',
+		handle: '',
+	})
 
-  const { handle, username, email, password, password2 } = registerState;
+	const { handle, username, email, password, password2 } = registerState
 
-  const onChange = (e) =>
-    setRegisterState({ ...registerState, [e.target.name]: e.target.value });
+	const onChange = (e) =>
+		setRegisterState({ ...registerState, [e.target.name]: e.target.value })
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+	const onSubmit = (e) => {
+		e.preventDefault()
 
-    if (password !== password2) {
-      console.log("Password do not match");
-    } else {
-      dispatch(registerUser({ username, email, handle, password }));
-    }
-  };
+		if (password !== password2) {
+			console.log('Password do not match')
+		} else {
+			dispatch(registerUser({ username, email, handle, password }))
+		}
+	}
 
-  if (isAuthenticated) {
-    return <Redirect to='/blogs' />;
-  }
+	if (isAuthenticated) {
+		return <Redirect to='/blogs' />
+	}
 
-  return (
-    <Grid container justify='center' alignItems='center'>
-      <Grid
-        className={classes.registerContainer}
-        container
-        item
-        xs={6}
-        direction='column'
-        justify='center'
-        alignItems='center'>
-        <Grid item>
-          <Typography variant='h5'>Register</Typography>
-        </Grid>
-        <Grid
-          container
-          item
-          display='column'
-          justify='center'
-          alignItems='center'>
-          <form className={classes.root}>
-            <Grid item>
-              <TextField
-                id='username'
-                name='username'
-                label='Username'
-                variant='outlined'
-                onChange={onChange}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id='email'
-                name='email'
-                label='Email'
-                variant='outlined'
-                onChange={onChange}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id='handle'
-                name='handle'
-                label='handle'
-                variant='outlined'
-                onChange={onChange}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id='password'
-                name='password'
-                label='password'
-                variant='outlined'
-                onChange={onChange}
-              />
-            </Grid>
-            <Grid item>
-              <TextField
-                id='password2'
-                name='password2'
-                label='re-enter password'
-                variant='outlined'
-                onChange={onChange}
-              />
-            </Grid>
-          </form>
-        </Grid>
-        <Grid item>
-          <Button
-            color='primary'
-            variant='contained'
-            type='submit'
-            onClick={onSubmit}>
-            Let's Get Started
-          </Button>
-        </Grid>
-        <Grid item>
-          <Typography variant='subtitle2' className={classes.registerContainer}>
-            Don't have an account? <Link to='/login'>Sign Up</Link>
-          </Typography>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
-};
+	return (
+		<Grid container justify='center'>
+			<Grid item xs={10} md={6} className={classes.parentGrid}>
+				<Paper elevation={10} className={classes.paper}>
+					<Grid container justify='center' alignItems='center'>
+						<Grid
+							className={classes.registerContainer}
+							container
+							item
+							xs={6}
+							direction='column'
+							justify='center'
+							alignItems='center'>
+							<Grid item>
+								<Typography variant='h5' className={classes.typography}>
+									Register
+								</Typography>
+							</Grid>
+							<Grid
+								container
+								item
+								display='column'
+								justify='center'
+								alignItems='center'>
+								<form className={classes.root}>
+									<Grid item>
+										<TextField
+											id='username'
+											name='username'
+											label='Username'
+											variant='outlined'
+											onChange={onChange}
+											focused={false}
+											classes={{
+												root: classes.textfieldColor,
+											}}
+										/>
+									</Grid>
+									<Grid item>
+										<TextField
+											id='email'
+											name='email'
+											label='Email'
+											variant='outlined'
+											onChange={onChange}
+											focused={false}
+											classes={{
+												root: classes.textfieldColor,
+											}}
+										/>
+									</Grid>
+									<Grid item>
+										<TextField
+											id='handle'
+											name='handle'
+											label='handle'
+											variant='outlined'
+											onChange={onChange}
+											focused={false}
+											classes={{
+												root: classes.textfieldColor,
+											}}
+										/>
+									</Grid>
+									<Grid item>
+										<TextField
+											id='password'
+											name='password'
+											label='password'
+											variant='outlined'
+											onChange={onChange}
+											focused={false}
+											classes={{
+												root: classes.textfieldColor,
+											}}
+										/>
+									</Grid>
+									<Grid item>
+										<TextField
+											id='password2'
+											name='password2'
+											label='re-enter password'
+											variant='outlined'
+											onChange={onChange}
+											focused={false}
+											classes={{
+												root: classes.textfieldColor,
+											}}
+										/>
+									</Grid>
+								</form>
+							</Grid>
+							<Grid item>
+								<Button
+									color='primary'
+									variant='contained'
+									type='submit'
+									onClick={onSubmit}>
+									Let's Get Started
+								</Button>
+							</Grid>
+							<Grid item>
+								<Typography
+									variant='subtitle2'
+									align='center'
+									className={classes.footer}>
+									Already have an account?{' '}
+									<Link to='/login'>
+										<span className={classes.link}> Sign In</span>
+									</Link>
+								</Typography>
+							</Grid>
+						</Grid>
+					</Grid>
+				</Paper>
+			</Grid>
+		</Grid>
+	)
+}
 
-export default Login;
+export default Login
