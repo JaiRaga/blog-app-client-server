1a3d1d040b2b36cfd72dba4b5d48f6e7ac7dcf99
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-03 12:49:37 +0530

    added state to indicate empty blogs returned

--- client/src/redux/reducers/blog.js
+++ client/src/redux/reducers/blog.js
@@ -1,60 +1,71 @@
 import {
-  UPDATE_TRENDING,
-  TRENDING_ERROR,
-  GET_BLOGS,
-  BLOG_ERROR,
-  POST_BLOG
-} from "../actions/types";
+	UPDATE_TRENDING,
+	TRENDING_ERROR,
+	GET_BLOGS,
+	BLOG_ERROR,
+	POST_BLOG,
+	BLOGS_NOT_FOUND,
+} from '../actions/types'
 
 const initialState = {
-  blog: null,
-  search: [],
-  blogs: [],
-  trending: [],
-  openTrending: false,
-  loading: true,
-  errors: {}
-};
+	blog: null,
+	search: [],
+	blogs: [],
+	trending: [],
+	openTrending: false,
+	loading: true,
+	notFound: false,
+	errors: {},
+}
 
 export default (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case GET_BLOGS:
-      return {
-        ...state,
-        blogs: [...payload],
-        loading: false
-      };
+	const { type, payload } = action
+	switch (type) {
+		case GET_BLOGS:
+			return {
+				...state,
+				blogs: [...payload],
+				notFound: false,
+				loading: false,
+			}
 
-    case POST_BLOG:
-      return {
-        ...state,
-        blogs: [payload, ...state.blogs],
-        loading: false
-      };
+		case POST_BLOG:
+			return {
+				...state,
+				blogs: [payload, ...state.blogs],
+				loading: false,
+			}
 
-    case BLOG_ERROR:
-      return {
-        ...state,
-        errors: { blogsError: true },
-        loading: false
-      };
+		case BLOG_ERROR:
+			return {
+				...state,
+				errors: { blogsError: true },
+				loading: false,
+			}
 
-    case UPDATE_TRENDING:
-      return {
-        ...state,
-        openTrending: payload,
-        loading: false
-      };
+		case BLOGS_NOT_FOUND:
+			return {
+				...state,
+				notFound: true,
+				blogs: [],
+				loading: false,
+			}
 
-    case TRENDING_ERROR:
-      return {
-        ...state,
-        error: { trendingError: true },
-        loading: false
-      };
+		case UPDATE_TRENDING:
+			return {
+				...state,
+				openTrending: payload,
+				loading: false,
+			}
 
-    default:
-      return state;
-  }
-};
+		case TRENDING_ERROR:
+			return {
+				...state,
+				error: { trendingError: true },
+				loading: false,
+			}
+
+		default:
+			return state
+	}
+}
