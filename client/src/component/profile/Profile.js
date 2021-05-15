86587652e95a1186d1eb3f7e2888c0102781d260
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-15 14:40:47 +0530

    added no of blogs created by the user

--- client/src/component/profile/Profile.js
+++ client/src/component/profile/Profile.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Grid, Avatar } from '@material-ui/core'
+import React, { useEffect } from 'react'
+import { Grid, Avatar, CircularProgress } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { getBlogsByMe } from '../../redux/actions/blog'
 import pic from '../../img/pic.jpg'
 
 const useStyles = makeStyles((theme) => ({
@@ -18,8 +19,10 @@ const useStyles = makeStyles((theme) => ({
 
 const Profile = () => {
 	const classes = useStyles()
+	const dispatch = useDispatch()
 
 	const user = useSelector((state) => state.auth.user)
+	const blogs = useSelector((state) => state.blog.currentUserBlogs)
 
 	return (
 		<Grid
@@ -30,16 +33,16 @@ const Profile = () => {
 			alignItems='center'
 			className={classes.grid}>
 			<Grid container item justify='space-between' alignItems='center'>
-				<Avatar className={classes.avatar} alt='pic' src={pic} />
+				<Avatar className={classes.avatar} alt='pic' src={user.avatar || pic} />
 				<Grid item className={classes.gridRight}>
 					<Grid container item direction='column'>
 						<Grid item>{user.username}</Grid>
-						<Grid item>rating</Grid>
+						<Grid item>Rating: {!user.rating ? 'NA' : user.rating}</Grid>
 					</Grid>
 				</Grid>
 			</Grid>
 			<Grid container item direction='column' className={classes.margin}>
-				<Grid item>Blogs</Grid>
+				<Grid item>Blogs: {blogs.length === 0 ? 'NA' : blogs.length}</Grid>
 				<Grid item>Trending</Grid>
 				<Grid item>Suggestions</Grid>
 				<Grid item>Tags</Grid>
