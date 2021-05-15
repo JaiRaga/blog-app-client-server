ace303a77d7910b0770035264e5b04bd41f0125e
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-15 14:37:57 +0530

    passed blogs to profile display component as props

--- client/src/component/profile/ProfileContainer.js
+++ client/src/component/profile/ProfileContainer.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
-import { Grid, Hidden } from '@material-ui/core'
+import React, { useEffect, useState, Fragment } from 'react'
+import { CircularProgress, Grid, Hidden } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Profile from './Profile'
 import ProfileDisplay from './ProfileDisplay'
 import PageNavigation from '../layout/PageNavigation'
 import { loadUser } from '../../redux/actions/auth'
+import { getBlogsByMe } from '../../redux/actions/blog'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -20,6 +21,9 @@ const useStyles = makeStyles((theme) => ({
 		// margin: 5,
 		padding: 20,
 	},
+	loading: {
+		marginTop: 20,
+	},
 	profileContent: {
 		marginLeft: 0,
 	},
@@ -31,21 +35,33 @@ const ProfileContainer = () => {
 
 	useEffect(() => {
 		dispatch(loadUser())
+		dispatch(getBlogsByMe())
 	}, [])
+
+	const user = useSelector((state) => state.auth.user)
+	const blogs = useSelector((state) => state.blog.currentUserBlogs)
 
 	return (
 		<Grid container justify='center' className={classes.root}>
-			<PageNavigation />
-			<Grid item xs={12} sm={6} lg={8} className={classes.profileContent}>
-				<Grid container item justify='center'>
-					<Grid item xs={5} md={3} className={classes.grid}>
-						<Profile />
-					</Grid>
-					<Grid item className={classes.grid} xs={12} md={6}>
-						<ProfileDisplay />
-					</Grid>
+			{!user ? (
+				<Grid item className={classes.loading}>
+					<CircularProgress thickness={5} />
 				</Grid>
-			</Grid>
+			) : (
+				<Fragment>
+					<PageNavigation />
+					<Grid item xs={12} sm={6} lg={8} className={classes.profileContent}>
+						<Grid container item justify='center'>
+							<Grid item xs={5} md={3} className={classes.grid}>
+								<Profile />
+							</Grid>
+							<Grid item className={classes.grid} xs={12} md={6}>
+								<ProfileDisplay blogs={blogs} />
+							</Grid>
+						</Grid>
+					</Grid>
+				</Fragment>
+			)}
 		</Grid>
 	)
 }
