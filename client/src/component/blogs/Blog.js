27db7b3f69fc252f70d176a9662758596cce294e
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-15 18:00:11 +0530

    added condition to pull data from local storage in case of page refresh

--- client/src/component/blogs/Blog.js
+++ client/src/component/blogs/Blog.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
 import {
 	Grid,
 	Typography,
 	IconButton,
 	Tooltip,
 	Divider,
+	CircularProgress,
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import { useSelector, useDispatch } from 'react-redux'
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
 	root: {
 		display: 'flex',
 		backgroundColor: theme.palette.background.default,
+	},
+	loading: {
+		marginTop: 20,
 	},
 	divider: {
 		width: '100%',
@@ -62,68 +66,79 @@ const Blog = (prop) => {
 	const classes = useStyles()
 	// const history = useHistory()
 	// const location = useLocation()
-	const blogs = useSelector((state) => state.blog.blogs)
+	let blogs = useSelector((state) => state.blog.blogs)
+
+	if (blogs.length === 0) blogs = JSON.parse(localStorage.getItem('getBlogs'))
+
 	let blog = blogs.filter((blog) => blog._id === prop.match.params.id)
 	// console.log(prop.match.params.id, blog)
 	blog = blog[0]
 
 	return (
-		<Grid
-			container
-			className={classes.root}
-			direction='column'
-			justify='center'
-			alignItems='center'>
-			<Grid item>
-				<Typography variant='h1'>{blog.title}</Typography>
-			</Grid>
-			<Grid item>
-				<Grid container item>
-					<Grid item className={classes.paddingVerified}>
-						<CheckIcon />
-					</Grid>
-					<Grid item className={classes.verifiedText}>
-						<Typography variant='subtitle2'>
-							{blog.readtime} mins read
-						</Typography>
-					</Grid>
+		<Fragment>
+			{!blog ? (
+				<Grid container justify='center' className={classes.loading}>
+					<CircularProgress />
 				</Grid>
-			</Grid>
-			<Grid item>
-				<Grid container item justify='flex-start' alignItems='center'>
-					<Grid item className={classes.visibilityIcon}>
-						<Tooltip title='Views' arrow placement='left'>
-							<VisibilityIcon />
-						</Tooltip>
+			) : (
+				<Grid
+					container
+					className={classes.root}
+					direction='column'
+					justify='center'
+					alignItems='center'>
+					<Grid item>
+						<Typography variant='h1'>{blog.title}</Typography>
 					</Grid>
 					<Grid item>
-						<Typography variant='subtitle1'>10008 Views</Typography>
+						<Grid container item>
+							<Grid item className={classes.paddingVerified}>
+								<CheckIcon />
+							</Grid>
+							<Grid item className={classes.verifiedText}>
+								<Typography variant='subtitle2'>
+									{blog.readtime} mins read
+								</Typography>
+							</Grid>
+						</Grid>
 					</Grid>
-					<Grid item className={classes.margin}>
-						<Tooltip title='Download' arrow placement='right-start'>
-							<IconButton aria-label='download'>
-								<GetAppIcon />
-							</IconButton>
-						</Tooltip>
+					<Grid item>
+						<Grid container item justify='flex-start' alignItems='center'>
+							<Grid item className={classes.visibilityIcon}>
+								<Tooltip title='Views' arrow placement='left'>
+									<VisibilityIcon />
+								</Tooltip>
+							</Grid>
+							<Grid item>
+								<Typography variant='subtitle1'>10008 Views</Typography>
+							</Grid>
+							<Grid item className={classes.margin}>
+								<Tooltip title='Download' arrow placement='right-start'>
+									<IconButton aria-label='download'>
+										<GetAppIcon />
+									</IconButton>
+								</Tooltip>
+							</Grid>
+						</Grid>
 					</Grid>
-				</Grid>
-			</Grid>
-			<Grid item className={classes.blogBody} md={6}>
-				<Typography variant='h6'>{blog.body}</Typography>
-			</Grid>
-			<Divider className={classes.divider} />
-			<Grid item className={classes.comments}>
-				<Grid container item direction='column' justify='flex-end'>
-					<Typography variant='h3' className={classes.commentTitle}>
-						Comments
-					</Typography>
-					<Comments />
-				</Grid>
-			</Grid>
-			{/* <Grid item className={classes.footer}>
+					<Grid item className={classes.blogBody} md={6}>
+						<Typography variant='h6'>{blog.body}</Typography>
+					</Grid>
+					<Divider className={classes.divider} />
+					<Grid item className={classes.comments}>
+						<Grid container item direction='column' justify='flex-end'>
+							<Typography variant='h3' className={classes.commentTitle}>
+								Comments
+							</Typography>
+							<Comments />
+						</Grid>
+					</Grid>
+					{/* <Grid item className={classes.footer}>
         <Footer />
       </Grid> */}
-		</Grid>
+				</Grid>
+			)}
+		</Fragment>
 	)
 }
 
