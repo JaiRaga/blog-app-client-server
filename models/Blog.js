a52f6dbcb2828d767bdbfd6ea8c903ba52e912d1
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-16 21:33:10 +0530

    added comments field

--- models/Blog.js
+++ models/Blog.js
@@ -1,46 +1,63 @@
-const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
 const blogsSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    body: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    category: [{ type: String, required: true, trim: true }],
-    readtime: {
-      type: Number
-    },
-    stars: {
-      type: Number
-    },
-    owner: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: "User"
-    }
-  },
-  {
-    timestamps: true
-  }
-);
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		body: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		category: [{ type: String, required: true, trim: true }],
+		readtime: {
+			type: Number,
+		},
+		stars: {
+			type: Number,
+		},
+		owner: {
+			type: Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		comments: [
+			{
+				user: {
+					type: Schema.Types.ObjectId,
+					ref: 'User',
+				},
+				text: {
+					type: String,
+				},
+				date: {
+					type: Date,
+					default: Date.now,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+)
 
-blogsSchema.pre("save", async function (next) {
-  const blog = this;
+blogsSchema.pre('save', async function (next) {
+	const blog = this
 
-  blog.isModified("body")
-    ? (blog.readtime = Math.round(blog.body.length / 200))
-    : null;
+	blog.isModified('body')
+		? (blog.readtime = Math.round(blog.body.length / 200))
+		: null
 
-  next();
-});
+	// blog.populate('owner').populate({ path: 'comments.user' }).execPopulate()
 
-const Blog = mongoose.model("Blog", blogsSchema);
+	next()
+})
 
-module.exports = Blog;
+const Blog = mongoose.model('Blog', blogsSchema)
+
+module.exports = Blog
