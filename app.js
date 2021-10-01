a70eebfbe715657c2565b4f6db0cfb623db8f5a9
JaiRaga <raga.j.santhosh@gmail.com>
2021-10-01 10:48:53 +0530

    :fire: connected to client on production

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-console.log("sarvamum Krishnan Arpanam");
+console.log('sarvamum Krishnan Arpanam')
 
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-require("./mongoose/mongoose");
-const usersRouter = require("./routes/api/users");
-const blogsRouter = require("./routes/api/blogs");
+require('./mongoose/mongoose')
+const usersRouter = require('./routes/api/users')
+const blogsRouter = require('./routes/api/blogs')
 
-app.use(express.json());
+app.use(express.json())
 
-const port = process.env.PORT || 9008;
+app.use('/api', usersRouter)
+app.use('/api', blogsRouter)
 
-app.use("/api", usersRouter);
-app.use("/api", blogsRouter);
+// Serve static assests in production
+if (process.env.NODE_ENV === 'production') {
+	// Serve static folder
+	app.use(express.static('client/build'))
 
-app.listen(port, console.log(`Running on Port ${port}`));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
+const port = process.env.PORT || 9008
+
+app.listen(port, console.log(`Running on Port ${port}`))
